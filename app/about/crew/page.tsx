a5f6d48f18dc3049bea79043541eb6247afe2e2e
Tien Dat Pham <dat20036@gmail.com>
2025-04-06 13:19:32 +0700

    feat: add crew page stubs.

---
+++ app/about/crew/page.tsx
@@ -0,0 +1,26 @@
+// import { VNS_Member } from "@/app/about/crew/index";
+import InProgress from "@/components/InProgress";
+
+// import MemberBox from "@/components/MemberBox";
+// import crewList from "@/data/crew.json";
+
+export default function CrewPage() {
+    // const members: VNS_Member[] = crewList.members;
+    {
+        /*
+        {members.map((member) => {
+                return (
+                    <MemberBox
+                        key={member.name}
+                        image={member.image}
+                        name={member.name}
+                        role={member.role}
+                        quote={member.quote}
+                    />
+                );
+            })}
+    */
+    }
+
+    return <InProgress />;
+}
---
+++ components/InProgress.tsx
@@ -0,0 +1,3 @@
+export default function InProgress() {
+    return <div className={"text-2xl"}>Chúng tôi đang nấu...</div>;
+}
