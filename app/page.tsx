b1d107b9b256361c9ccbe62856bbcd9580397eb8
Tien Dat Pham <dat20036@gmail.com>
2025-04-06 22:14:05 +0700

    fix: make homepage `flex`.

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <>
+        <div className={"flex flex-col items-center"}>
             <Image className={"px-10"} src={VNS_Banner} alt={"VNS-banner"} width={650} />
             <span className={"py-10 text-2xl font-extrabold"}>Coming soon.</span>
-        </>
+        </div>
     );
 }
