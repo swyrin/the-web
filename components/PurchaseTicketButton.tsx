0934128cc0fc13e9f4a6c415270c6783e8e4e346
Tien Dat Pham <dat20036@gmail.com>
2025-04-06 22:13:28 +0700

    fix: add medium size selector for purchase ticket CTA.

--- components/PurchaseTicketButton.tsx
+++ components/PurchaseTicketButton.tsx
@@ -2,7 +2,7 @@ export default function PurchaseTicketButton() {
     return (
         <div
             className={
-                "btn sm:btn-sm lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
+                "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
             }
         >
             <span>Mua vé</span>
