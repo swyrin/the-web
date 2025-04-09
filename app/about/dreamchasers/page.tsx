73f73e01078881f4a17b635c4a44c8c75afc9923
Tien Dat Pham <dat20036@gmail.com>
2025-04-09 08:58:31 +0700

    fix: the site now works on mobile.

--- app/about/dreamchasers/page.tsx
+++ app/about/dreamchasers/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 function PartnerList() {
     return (
-        <div className={"w-220 items-center font-normal"}>
+        <div className={"items-center font-normal"}>
             <ul className={"list-disc space-y-5"}>
                 <li>
                     <b>Nói Không Với 0 Sanity</b>: Page chuyên cập nhật thông tin trên máy chủ EN, cũng như liệt kê &
@@ -8,21 +10,23 @@ function PartnerList() {
                 </li>
                 <li>
                     <b>Over Heaven</b>: Studio âm nhạc, thỉnh thoảng sản xuất các sản phẩm fanmade về Arknights. Đứng
-                    đầu bởi Huynh Kiet
+                    đầu bởi Huynh Kiet.
                 </li>
                 <li>
                     <b>Thư Viện Laterano</b>: Page chuyên phân tích và tóm tắt cốt truyện của tựa game. Chủ page: Tuan
-                    Anh Nguyen
+                    Anh Nguyen.
                 </li>
                 <li>
-                    <b>TTShop</b>: Tri Vu - nạp game
+                    <b>TTShop</b>: Tri Vu - nạp game.
                 </li>
                 <li>
-                    <b>Hiệu sách cuối phố của N</b>: hàng xóm của Trạm dừng chân chốn Terra trong mảng dịch thuật
+                    <b>Hiệu sách cuối phố của N</b>: hàng xóm của Trạm dừng chân chốn Terra trong mảng dịch thuật.
                 </li>
-                <li>Ổ nghiện Shitpost và Gacha</li>
                 <li>
-                    <b>Area 57</b>: <del>maimai vietnam station</del>
+                    <b>Ổ nghiện Shitpost và Gacha</b>
+                </li>
+                <li>
+                    <b>AREA 57</b>: <del>maimai vietnam station</del>
                 </li>
             </ul>
         </div>
@@ -31,39 +35,39 @@ function PartnerList() {
 
 export default function AboutDreamchasers() {
     return (
-        <div className={"py-10"}>
-            <div className={"text-center text-3xl"}>About the Dreamchasers</div>
-            <br></br>
-            <div className={"w-235 items-center font-normal"}>
+        <div className={"p-10 sm:w-100 md:w-200 lg:w-220"}>
+            <div className={"text-center text-3xl font-extrabold"}>About the Dreamchasers</div>
+            <br />
+            <div className={"items-center font-normal"}>
                 <p>
                     <b>Arknights Vietnam Station Dreamchasers</b> là một nhóm của{" "}
                     <b>Arknights Vietnam Station (A9VNS)</b> trực thuộc Vietnam Station Network, tổ chức các sự kiện
                     Offline/Tournament cho nhóm A9VNS.
                 </p>
-                <br></br>
-                <p>Arknights VNS hiện có các nền tảng khác để kết nối cộng đồng như sau:{"\n\n"}</p> <br></br>
-                <ul className={"ml-10 w-200 list-disc space-y-5"}>
+                <br />
+                <p>Arknights VNS hiện có các nền tảng khác để kết nối cộng đồng như sau:</p>
+                <br />
+                <ul className={"ml-10 list-disc space-y-5"}>
                     <li>
-                        <b>Tram dừng chân chốn Terra</b>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện
+                        <b>Trạm dừng chân chốn Terra</b>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện
                         tranh tranh được phát hành bởi Hypergryph (dưới tên Terra Historicus) hoặc fanart cộng đồng.
                         Ngoài ra, page thỉnh thoảng cũng sẽ cập nhật & chia sẻ các nội dung, tin tức liên quan đến
                         Arknights.
                     </li>
                     <li>
                         <b>「 Arknights VNS 」</b>- Máy chủ Discord chính thức của nhóm (
-                        <a href={"https://discord.gg/arknights-vns"} className={"underline"}>
+                        <Link href={"https://discord.gg/arknights-vns"} className={"underline"}>
                             discord.gg/arknights-vns
-                        </a>
+                        </Link>
                         ).
                     </li>
                 </ul>
             </div>
-            <br></br>
-            <br></br>
-            <div className={"text-center text-2xl"}>Đối tác phát triển</div>
-            <br></br>
+            <br />
+            <div className={"text-center text-2xl font-extrabold"}>Đối tác phát triển</div>
+            <br />
             <PartnerList />
-            <br></br>
+            <br />
         </div>
     );
 }
