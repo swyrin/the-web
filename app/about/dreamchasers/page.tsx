0c230cae552cf23a3a3e27fa5a402f3d714a64a3
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-04-09 09:17:04 +0700

    chore: new content by :cactus:

--- app/about/dreamchasers/page.tsx
+++ app/about/dreamchasers/page.tsx
@@ -1,73 +1,70 @@
-import Link from "next/link";
-
 function PartnerList() {
     return (
-        <div className={"items-center font-normal"}>
-            <ul className={"list-disc space-y-5"}>
+        <div className={"w-220 items-center font-normal"}>
+            <ol className={"list-decimal space-y-5"}>
                 <li>
-                    <b>Nói Không Với 0 Sanity</b>: Page chuyên cập nhật thông tin trên máy chủ EN, cũng như liệt kê &
-                    phân tích chi tiết về Enemies và Operators.
+                    <a href={"https://www.facebook.com/ngan.tinh.3958"} className={"underline"}>
+                        <b>Phổ Lang</b>
+                    </a>
+                    : Chủ nhóm Arknights&apos;s Goods - Buôn thận và cày ruộng cùng thú 4 tai
                 </li>
                 <li>
-                    <b>Over Heaven</b>: Studio âm nhạc, thỉnh thoảng sản xuất các sản phẩm fanmade về Arknights. Đứng
-                    đầu bởi Huynh Kiet.
+                    <a href={"https://www.facebook.com/rei.naoca.7"} className={"underline"}>
+                        <b>Rei Não Cá</b>
+                    </a>
+                    : Trực thuộc circle Văn phòng Vã
                 </li>
                 <li>
-                    <b>Thư Viện Laterano</b>: Page chuyên phân tích và tóm tắt cốt truyện của tựa game. Chủ page: Tuan
-                    Anh Nguyen.
+                    <a href={"https://www.facebook.com/shinsei.hijiri"} className={"underline"}>
+                        <b>Võ Thị Bi</b>
+                    </a>
+                    : Trực thuộc circle 0 Sanity Dokutah
                 </li>
                 <li>
-                    <b>TTShop</b>: Tri Vu - nạp game.
+                    <a href={"https://linktr.ee/vncommunityleague"} className={"underline"}>
+                        <b>Vietnam Community League</b>
+                    </a>
+                    : Hệ thống giải osu! chuyên nghiệp đầu tiên tại Việt Nam.
                 </li>
-                <li>
-                    <b>Hiệu sách cuối phố của N</b>: hàng xóm của Trạm dừng chân chốn Terra trong mảng dịch thuật.
-                </li>
-                <li>
-                    <b>Ổ nghiện Shitpost và Gacha</b>
-                </li>
-                <li>
-                    <b>AREA 57</b>: <del>maimai vietnam station</del>
-                </li>
-            </ul>
+            </ol>
         </div>
     );
 }
 
 export default function AboutDreamchasers() {
     return (
-        <div className={"p-10 sm:w-100 md:w-200 lg:w-220"}>
-            <div className={"text-center text-3xl font-extrabold"}>About the Dreamchasers</div>
-            <br />
-            <div className={"items-center font-normal"}>
+        <div className={"py-10"}>
+            <div className={"text-center text-3xl"}>About the Dreamchasers</div>
+            <br></br>
+            <div className={"w-235 items-center font-normal"}>
                 <p>
-                    <b>Arknights Vietnam Station Dreamchasers</b> là một nhóm của{" "}
-                    <b>Arknights Vietnam Station (A9VNS)</b> trực thuộc Vietnam Station Network, tổ chức các sự kiện
-                    Offline/Tournament cho nhóm A9VNS.
+                    Dreamchasers, là một đội ngũ chuyên về làm sự kiện Offline, trực thuộc cộng đồng <a href={"https://www.facebook.com/groups/arknights.vietnam.station"} className={"underline"}><b>Arknights Vietnam Station [Arknights VNS]</b></a>
+                    , đồng thời cũng là một trong những nhánh của Station Network / Project VNS
                 </p>
-                <br />
-                <p>Arknights VNS hiện có các nền tảng khác để kết nối cộng đồng như sau:</p>
-                <br />
-                <ul className={"ml-10 list-disc space-y-5"}>
+                <br></br>
+                <p>Arknights VNS hiện có các nền tảng khác để kết nối cộng đồng trong Project VNS như::{"\n\n"}</p>
+                <br></br>
+                <ul className={"ml-10 w-200 list-disc space-y-5"}>
                     <li>
-                        <b>Trạm dừng chân chốn Terra</b>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện
-                        tranh tranh được phát hành bởi Hypergryph (dưới tên Terra Historicus) hoặc fanart cộng đồng.
-                        Ngoài ra, page thỉnh thoảng cũng sẽ cập nhật & chia sẻ các nội dung, tin tức liên quan đến
-                        Arknights.
+                        <a href={"https://www.facebook.com/terrastationvn"} className = {"underline"}><b>Tram dừng chân chốn Terra / @terrastationvn </b></a>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện tranh được phát hành bởi{" "}
+                        Hypergryph (dưới tên Terra Historicus) hoặc fanart cộng đồng. Ngoài ra, page thỉnh thoảng cũng
+                        sẽ cập nhật & chia sẻ các nội dung, tin tức liên quan đến Arknights .
                     </li>
                     <li>
                         <b>「 Arknights VNS 」</b>- Máy chủ Discord chính thức của nhóm (
-                        <Link href={"https://discord.gg/arknights-vns"} className={"underline"}>
+                        <a href={"https://discord.gg/arknights-vns"} className={"underline"}>
                             discord.gg/arknights-vns
-                        </Link>
+                        </a>
                         ).
                     </li>
                 </ul>
             </div>
-            <br />
-            <div className={"text-center text-2xl font-extrabold"}>Đối tác phát triển</div>
-            <br />
+            <br></br>
+            <br></br>
+            <div className={"text-center text-2xl"}>Đối tác phát triển</div>
+            <br></br>
             <PartnerList />
-            <br />
+            <br></br>
         </div>
     );
 }
