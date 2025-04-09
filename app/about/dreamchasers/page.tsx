4c324bd8024cfa27faa33ab265312c50b532d9b4
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-04-09 09:23:49 +0700

    fix: change all <a> usage to nextjs link

--- app/about/dreamchasers/page.tsx
+++ app/about/dreamchasers/page.tsx
@@ -1,29 +1,31 @@
+import Link from "next/link";
+
 function PartnerList() {
     return (
         <div className={"ml-5 w-220 items-center font-normal"}>
             <ol className={"list-decimal space-y-5"}>
                 <li>
-                    <a href={"https://www.facebook.com/ngan.tinh.3958"} className={"underline"}>
+                    <Link href={"https://www.facebook.com/ngan.tinh.3958"} className={"underline"}>
                         <b>Phổ Lang</b>
-                    </a>
+                    </Link>
                     : Chủ nhóm Arknights&apos;s Goods - Buôn thận và cày ruộng cùng thú 4 tai
                 </li>
                 <li>
-                    <a href={"https://www.facebook.com/rei.naoca.7"} className={"underline"}>
+                    <Link href={"https://www.facebook.com/rei.naoca.7"} className={"underline"}>
                         <b>Rei Não Cá</b>
-                    </a>
+                    </Link>
                     : Trực thuộc circle Văn phòng Vã
                 </li>
                 <li>
-                    <a href={"https://www.facebook.com/shinsei.hijiri"} className={"underline"}>
+                    <Link href={"https://www.facebook.com/shinsei.hijiri"} className={"underline"}>
                         <b>Võ Thị Bi</b>
-                    </a>
+                    </Link>
                     : Trực thuộc circle 0 Sanity Dokutah
                 </li>
                 <li>
-                    <a href={"https://linktr.ee/vncommunityleague"} className={"underline"}>
+                    <Link href={"https://linktr.ee/vncommunityleague"} className={"underline"}>
                         <b>Vietnam Community League</b>
-                    </a>
+                    </Link>
                     : Hệ thống giải osu! chuyên nghiệp đầu tiên tại Việt Nam.
                 </li>
             </ol>
@@ -38,7 +40,7 @@ export default function AboutDreamchasers() {
             <br></br>
             <div className={"w-235 items-center font-normal"}>
                 <p>
-                    Dreamchasers, là một đội ngũ chuyên về làm sự kiện Offline, trực thuộc cộng đồng <a href={"https://www.facebook.com/groups/arknights.vietnam.station"} className={"underline"}><b>Arknights Vietnam Station [Arknights VNS]</b></a>
+                    Dreamchasers, là một đội ngũ chuyên về làm sự kiện Offline, trực thuộc cộng đồng <Link href={"https://www.facebook.com/groups/arknights.vietnam.station"} className={"underline"}><b>Arknights Vietnam Station [Arknights VNS]</b></Link>
                     , đồng thời cũng là một trong những nhánh của Station Network / Project VNS
                 </p>
                 <br></br>
@@ -46,15 +48,15 @@ export default function AboutDreamchasers() {
                 <br></br>
                 <ul className={"ml-10 w-200 list-disc space-y-5"}>
                     <li>
-                        <a href={"https://www.facebook.com/terrastationvn"} className = {"underline"}><b>Tram dừng chân chốn Terra / @terrastationvn</b></a>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện tranh được phát hành bởi{" "}
+                        <Link href={"https://www.facebook.com/terrastationvn"} className = {"underline"}><b>Tram dừng chân chốn Terra / @terrastationvn</b></Link>: Fanpage chính thức của nhóm, là một team chuyên dịch truyện tranh được phát hành bởi{" "}
                         Hypergryph (dưới tên Terra Historicus) hoặc fanart cộng đồng. Ngoài ra, page thỉnh thoảng cũng
                         sẽ cập nhật & chia sẻ các nội dung, tin tức liên quan đến Arknights .
                     </li>
                     <li>
                         <b>「 Arknights VNS 」</b>- Máy chủ Discord chính thức của nhóm (
-                        <a href={"https://discord.gg/arknights-vns"} className={"underline"}>
+                        <Link href={"https://discord.gg/arknights-vns"} className={"underline"}>
                             discord.gg/arknights-vns
-                        </a>
+                        </Link>
                         ).
                     </li>
                 </ul>
