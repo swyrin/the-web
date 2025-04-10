466265792e1e514cb09d0ff689968e9f8b82e5e6
Tien Dat Pham <dat20036@gmail.com>
2025-04-10 10:32:26 +0700

    feat: properly style and align crew entry items.
    (cherry picked from commit 280ff9dee23ac3a0ce88514c1420b2b5f6e4d351)

--- components/MemberBox.tsx
+++ components/MemberBox.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 export default function MemberBox(props: VNS_Member) {
     return (
-        <div className={"my-auto inline-grid h-96 w-56 p-10 text-center"}>
+        <div className={"my-auto h-75 w-60 p-8 text-center"}>
             <Image
-                className={"mx-auto object-center"}
+                className={"mx-auto rounded-full border-2 border-gray-400 shadow-md"}
                 src={`/crew/${props.name}.png`}
                 width={100}
                 height={100}
                 alt={"VNS_Crew"}
             />
-            <div className={"text-xl font-extrabold"}>{props.name}</div>
-            <div className={"text-2xl"}>{props.role.replace("_", " ")}</div>
-            <div className={"text-sm text-gray-600 italic"}>&#34;{props.quote}&#34;</div>
+            <div className={"py-3 text-xl font-extrabold"}>{props.name}</div>
+            <div className={"py-3 text-xl"}>{props.role.replace("_", " ")}</div>
+            <div className={"py-3 text-sm text-gray-600 italic"}>&#34;{props.quote}&#34;</div>
         </div>
     );
 }
