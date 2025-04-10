3b867cc1c60fa47e5c5915bb54ac9ef8e8202fc0
Tien Dat Pham <dat20036@gmail.com>
2025-04-10 09:21:55 +0700

    feat: add location *hint* in landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import LocationPinIcon from "@/components/LocationPinIcon";
 import VNS_Banner from "@/public/VNS_Banner.png";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className={"flex flex-col items-center"}>
+        <div className={"flex flex-col items-center space-y-8"}>
             <Image className={"px-10"} src={VNS_Banner} alt={"VNS-banner"} width={650} />
-            <span className={"py-10 text-2xl font-extrabold"}>Coming soon.</span>
+            <div className={"flex items-center text-2xl font-extrabold"}>
+                <div className={"px-5"}>
+                    <LocationPinIcon size={30} />
+                </div>{" "}
+                Đông Lào, trust me bro.
+            </div>
         </div>
     );
 }
