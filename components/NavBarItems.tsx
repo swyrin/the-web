c3b4e2b8b6c62080c4640cae5b914bee6af28bd5
Tien Dat Pham <dat20036@gmail.com>
2025-04-10 20:45:37 +0700

    fix: make navbar items responsive on mobile.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- components/NavBarItems.tsx
+++ components/NavBarItems.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavbarItems() {
     return (
-        <div className="flex items-center gap-20">
+        <div className="place-items-center-safe lg:grid lg:grid-cols-3">
             <li>
                 <Link href={"/about/dreamchasers"}>Chúng tôi là ai?</Link>
             </li>
