af83f404bf836b50589f0e12387e9b8a3ab1efd7
Tien Dat Pham <dat20036@gmail.com>
2025-04-13 10:18:25 +0700

    feat: add "interactivity" for PurchaseTicketButton.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- components/PurchaseTicketButton.tsx
+++ components/PurchaseTicketButton.tsx
@@ -1,11 +1,38 @@
+"use client";
+
+import Link from "next/link";
+
 export default function PurchaseTicketButton() {
     return (
-        <div
-            className={
-                "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
-            }
-        >
-            <span>Mua vé</span>
-        </div>
+        <>
+            <button
+                className={
+                    "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
+                }
+                // TODO: Wait for ticket opening date.
+                onClick={() => (document.getElementById("ticket_modal")! as HTMLDialogElement).showModal()}
+            >
+                <span>Mua vé</span>
+            </button>
+
+            <dialog id="ticket_modal" className="modal">
+                <div className="modal-box bg-white">
+                    <h3 className="text-lg font-bold">Hiện tại vé chưa mở bán</h3>
+                    <p className="py-4">
+                        Bạn hãy theo dõi fanpage{" "}
+                        <Link className={"font-bold underline"} href={"https://www.facebook.com/terrastationvn"}>
+                            Trạm dừng chân chốn Terra
+                        </Link>{" "}
+                        để cập nhật thông tin sớm nhất nhé!
+                        <br />
+                        <br />
+                        <span className={"text-sm italic"}>(Bấm bên ngoài để đóng)</span>
+                    </p>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button />
+                </form>
+            </dialog>
+        </>
     );
 }
