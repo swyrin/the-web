8b2ce470f8fa9fe22caf1d160a1fd7c772ccf1a8
Tien Dat Pham <dat20036@gmail.com>
2025-04-16 15:28:41 +0700

    feat!: (breaking change) the web now requires a dedicated server.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
-    output: "export",
 };
 
 export default nextConfig;
