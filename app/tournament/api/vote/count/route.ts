f9445c590f68f4bd067499f06df15bb0a540e63a
Tien Dat Pham <dat20036@gmail.com>
2025-04-16 15:54:06 +0700

    fix: fix my own "oversight".
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- app/tournament/api/vote/count/route.ts
+++ app/tournament/api/vote/count/route.ts
@@ -4,9 +4,19 @@ import { prismaClient } from "@/lib/prisma_client";
  * Get total vote count.
  */
 export async function GET() {
-    const entries = await prismaClient.memberVote.findMany({});
+    const entries = await prismaClient.memberVote.findMany({
+        select: {
+            count: true,
+        },
+    });
+
+    let voteCount = 0;
+
+    entries.forEach((x) => {
+        voteCount += x.count;
+    });
 
     return Response.json({
-        count: entries.length,
+        count: voteCount,
     });
 }
