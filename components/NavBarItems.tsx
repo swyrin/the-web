c749441fdb4f1abeb360bd7d7ffc31a004a6c791
Tien Dat Pham <dat20036@gmail.com>
2025-04-16 13:01:36 +0700

    feat: highlight active item in navbar.

--- components/NavBarItems.tsx
+++ components/NavBarItems.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavbarItems() {
+    const path = usePathname();
+
+    // https://stackoverflow.com/questions/53262263/target-active-link-when-the-route-is-active-in-next-js
+    // https://nextjs.org/docs/app/api-reference/functions/use-router#migrating-from-nextrouter
+
     return (
         <div className="lg:grid lg:grid-cols-3 lg:place-items-center-safe">
-            <li>
+            <li className={path == "/about/dreamchasers" ? "rounded-sm bg-black text-white" : ""}>
                 <Link href={"/about/dreamchasers"}>Chúng tôi là ai?</Link>
             </li>
-            <li>
+            <li className={path == "/about/crew" ? "rounded-sm bg-black text-white" : ""}>
                 <Link href={"/about/crew"}>Nhân sự</Link>
             </li>
-            <li>
+            <li className={path == "/timeline" ? "rounded-sm bg-black text-white" : ""}>
                 <Link href={"/timeline"}>Kế hoạch</Link>
             </li>
         </div>
