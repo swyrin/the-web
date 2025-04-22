54afc9e25d06ba8e8a4b636de201e6f5352cd47f
Tien Dat Pham <dat20036@gmail.com>
2025-04-22 08:54:38 +0700

    feat(ticket): proper time-gate implementation.

--- components/PurchaseTicketButton.tsx
+++ components/PurchaseTicketButton.tsx
@@ -1,23 +1,37 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function PurchaseTicketButton() {
+    const router = useRouter();
+
+    // TODO: Wait for ticket opening date.
+    function TicketNavigation() {
+        const currentDate = new Date(Date.now());
+        const ticketOpening = new Date("2025-07-27");
+
+        if (currentDate < ticketOpening) {
+            (document.getElementById("ticket_modal")! as HTMLDialogElement).showModal();
+        } else {
+            router.push("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+
     return (
         <>
             <button
                 className={
                     "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
                 }
-                // TODO: Wait for ticket opening date.
-                onClick={() => (document.getElementById("ticket_modal")! as HTMLDialogElement).showModal()}
+                onClick={TicketNavigation}
             >
                 <span>Mua vé</span>
             </button>
 
             <dialog id="ticket_modal" className="modal">
                 <div className="modal-box bg-white">
-                    <h3 className="text-lg font-bold">Hiện tại vé chưa mở bán</h3>
+                    <h3 className="text-lg font-bold">Hiện tại vé chưa mở bán.</h3>
                     <p className="py-4">
                         Bạn hãy theo dõi fanpage{" "}
                         <Link className={"font-bold underline"} href={"https://www.facebook.com/terrastationvn"}>
@@ -26,7 +40,7 @@ export default function PurchaseTicketButton() {
                         để cập nhật thông tin sớm nhất nhé!
                         <br />
                         <br />
-                        <span className={"text-sm italic"}>(Bấm bên ngoài để đóng)</span>
+                        <span className={"text-sm italic"}>(Bấm ra bên ngoài để đóng)</span>
                     </p>
                 </div>
                 <form method="dialog" className="modal-backdrop">
