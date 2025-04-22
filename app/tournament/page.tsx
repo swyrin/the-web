076518626952b75331d09df3775d7c85fda64750
Tien Dat Pham <dat20036@gmail.com>
2025-04-22 09:16:17 +0700

    fix: use existing InProgress component in tournament landing page.

--- app/tournament/page.tsx
+++ app/tournament/page.tsx
@@ -1,7 +1,9 @@
+import InProgress from "@/components/InProgress";
+
 export default function Home() {
     return (
         <div className={"flex flex-col items-center space-y-8"}>
-            <h1 className="center text-2xl">We are cooking, again</h1>
+            <InProgress />
         </div>
     );
 }
