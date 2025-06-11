ea7f5fd4c25ac983bb11ab16a589899613630496
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-06-11 17:54:39 +0700

    chore: update timeline les go

--- app/(main)/timeline/page.tsx
+++ app/(main)/timeline/page.tsx
@@ -106,7 +106,7 @@ export default function EventTimeLine() {
                     />
 
                     <TimelineItem
-                        date={"2025-08-??"}
+                        date={"2025-08-10"}
                         data={"Offline #2: Dreamchasers"}
                         href={""}
                         finished={false}
