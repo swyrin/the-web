5a4df38db0f31a7c1be8aaa986f5b706e4bae216
Tien Dat Pham <dat20036@gmail.com>
2025-06-12 19:04:08 +0700

    fix: *sighs*.

--- components/MemberBox.tsx
+++ components/MemberBox.tsx
@@ -1,4 +1,4 @@
-import { VNS_Member } from "@/lib/vns_types";
+import { VNS_Member } from "@/lib/types/vns_member";
 import Image from "next/image";
 
 export default function MemberBox(props: VNS_Member) {
