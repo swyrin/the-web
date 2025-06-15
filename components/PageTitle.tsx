bb12cedf783611878628aa4d4455193663632a46
Tien Dat Pham <dat20036@gmail.com>
2025-06-15 22:12:12 +0700

    feat: use svg for page title decoration.

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,32 +1,31 @@
-import TitleDecoLeft from "@/public/title/title_decor_left.png";
-import TitleDecoRight from "@/public/title/title_decor_right.png";
-import Image from "next/image";
+import TitleDecorLeft from "@/components/svg/TitleDecorLeft";
+import TitleDecorRight from "@/components/svg/TitleDecorRight";
 
 type PageTitleProps = {
     title: string;
     favorText: string;
-    light?: boolean;
+    dark?: boolean;
 };
 
 export default function PageTitle(props: PageTitleProps) {
-    const isLightBg = props.light === true;
+    const isDarkBg = props.dark === true;
+    const invertStyle = isDarkBg ? "invert" : "";
+    const favorTextStyle = isDarkBg ? "text-white" : "text-black";
 
     return (
         <>
-            <div className={"m-8 flex items-center justify-center text-center"}>
-                <Image
-                    src={TitleDecoLeft}
-                    className={`${isLightBg ? "invert" : ""} mr-12 hidden md:inline`}
-                    alt={"Title Decoration Left"}
-                />
-                <h1 className={"text-center text-6xl font-extrabold"}>{props.title}</h1>
-                <Image
-                    src={TitleDecoRight}
-                    className={`${isLightBg ? "invert" : ""} ml-12 hidden md:inline`}
-                    alt={"Title Decoration Left"}
-                />
+            <div className={`mt-4 flex items-center justify-center text-center ${invertStyle}`}>
+                <div className={"mr-5"}>
+                    <TitleDecorLeft width={115} height={24} />
+                </div>
+                <h1 className={"text-center text-2xl font-extrabold md:text-4xl lg:text-6xl"}>
+                    {props.title}
+                </h1>
+                <div className={"ml-5"}>
+                    <TitleDecorRight width={115} height={24} />
+                </div>
             </div>
-            <p>{props.favorText}</p>
+            <p className={`my-4 ${favorTextStyle}`}>{props.favorText}</p>
         </>
     );
 }
---
+++ components/svg/TitleDecorLeft.tsx
@@ -0,0 +1,44 @@
+type TitleDecorLeftProps = {
+    width: number;
+    height: number;
+};
+
+export default function TitleDecorLeft(props: TitleDecorLeftProps) {
+    return (
+        <svg
+            width={props.width}
+            height={props.height}
+            viewBox={`0 0 ${props.width} ${props.height}`}
+            fill={"none"}
+            xmlns={"http://www.w3.org/2000/svg"}
+        >
+            <path
+                fillRule={"evenodd"}
+                clipRule={"evenodd"}
+                d={
+                    "M106.38 8.65L103 0.986L99.62 8.65L91.953 12.03L99.62 15.41L103 23.078L106.38 15.41L114.047 12.03L106.38 8.65Z"
+                }
+                fill={"black"}
+            />
+            <path
+                fillRule={"evenodd"}
+                clipRule={"evenodd"}
+                d={
+                    "M62.1758 8.84792L59.5 2.78059L56.8242 8.84792L50.7545 11.5238L56.8242 14.1996L59.5 20.2701L62.1758 14.1996L68.2455 11.5238L62.1758 8.84792Z"
+                }
+                fill={"black"}
+            />
+            <path
+                fillRule={"evenodd"}
+                clipRule={"evenodd"}
+                d={
+                    "M26.0492 9.96459L24.5 6.45192L22.9508 9.96459L19.4368 11.5138L22.9508 13.0629L24.5 16.5774L26.0492 13.0629L29.5632 11.5138L26.0492 9.96459Z"
+                }
+                fill={"black"}
+            />
+            <line x1={"72"} y1={"11.5"} x2={"88"} y2={"11.5"} stroke={"black"} />
+            <line x1={"32"} y1={"11.5"} x2={"48"} y2={"11.5"} stroke={"black"} />
+            <line y1={"11.5"} x2={"16"} y2={"11.5"} stroke={"black"} />
+        </svg>
+    );
+}
