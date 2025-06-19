6070a1be531cfacdaa16512a1063555b52b83d39
Tien Dat Pham <dat20036@gmail.com>
2025-06-19 10:08:52 +0700

    fix(retro): fix the layout break on mobile.

--- components/RetroItem.tsx
+++ components/RetroItem.tsx
@@ -10,19 +10,19 @@ type RetroItemProps = {
 
 export default function RetroItem(props: RetroItemProps) {
     return (
-        <Link
-            href={props.href}
-            className={"mx-8 flex flex-col shadow-2xl/25 shadow-white lg:flex-row"}
-        >
-            <figure className={"flex h-48 max-w-full min-w-1/2 overflow-hidden"}>
+        <div className={"mx-8 flex flex-col shadow-2xl/25 shadow-white lg:flex-row"}>
+            <figure className={"flex max-h-48 max-w-full min-w-1/2 overflow-hidden"}>
                 <Image src={props.imageSrc} alt={"retro_image"} className={"self-center"} />
             </figure>
             <div className={"flex min-w-1/2 flex-col items-center justify-center bg-white"}>
-                <div className={"font-lg card-title m-2 text-center hover:underline"}>
+                <Link
+                    href={props.href}
+                    className={"font-lg card-title m-2 text-center hover:underline"}
+                >
                     {props.title}
-                </div>
-                <p className={"mb-3 text-center"}>{props.description}</p>
+                </Link>
+                <p className={"mx-4 mb-3 text-center"}>{props.description}</p>
             </div>
-        </Link>
+        </div>
     );
 }
