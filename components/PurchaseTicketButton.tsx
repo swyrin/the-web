6cc86c1e8fde79673f332442838d08da6fd9061c
Tien Dat Pham <dat20036@gmail.com>
2025-07-05 18:47:19 +0700

    feat!: lift the ticket date safeguard.

--- components/PurchaseTicketButton.tsx
+++ components/PurchaseTicketButton.tsx
@@ -1,22 +1,14 @@
-"use client";
-
 import Link from "next/link";
-
-import Text from "./Text/Text";
 
 export default function PurchaseTicketButton() {
     return (
-        <>
-            <Link
-                href={"/tickets"}
-                className={
-                    "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
-                }
-            >
-                <Text type={"title-4"} weight={600}>
-                    Mua vé
-                </Text>
-            </Link>
-        </>
+        <Link
+            href={"/tickets"}
+            className={
+                "btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"
+            }
+        >
+            <div className={"font-bold"}>Mua vé</div>
+        </Link>
     );
 }
