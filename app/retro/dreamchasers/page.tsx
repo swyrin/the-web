cd2a819a9b6ee5180becc38e4958ca9014dedd62
Tien Dat Pham <dat20036@gmail.com>
2025-07-17 22:43:13 +0700

    fix: bruh

--- app/retro/dreamchasers/page.tsx
+++ app/retro/dreamchasers/page.tsx
@@ -1,11 +1,7 @@
-import InProgress from "@/components/InProgress";
-
 export default function Dreamchasers() {
     return (
         <div className={"mx-auto p-10 sm:w-150 md:w-200 lg:w-250"}>
             <div className={"text-center text-3xl font-extrabold"}>Dreamchasers</div>
-            <br />
-            <InProgress></InProgress>
         </div>
     );
 }
