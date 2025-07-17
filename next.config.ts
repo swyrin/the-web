38b2c5f56a5da64930b91f3a32d69eddd4aa15e5
Tien Dat Pham <dat20036@gmail.com>
2025-07-17 08:57:40 +0700

    fix: add cors headers for the API. (#17)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    /* I don't need your cookie, for now. */
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
