601f733b2314dc97a77cb72d0183426b020bc50f
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-07-18 20:36:00 +0700

    fix: *sigh* eslint, again...

--- app/api/test/[cmd]/route.ts
+++ app/api/test/[cmd]/route.ts
@@ -34,7 +34,7 @@ export async function POST(
                 return NextResponse.json(
                     { pingStatus: "ok" },
                     { status: 200 },
-                )
+                );
         }
     } catch (error) {
         return NextResponse.json({ error });
