18d3841fd9427847a9768e89124136b6955b74fd
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-07-18 20:02:39 +0700

    feat: API test routes

---
+++ app/api/test/[cmd]/route.ts
@@ -0,0 +1,42 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+type testCommand = "auth" | "ping";
+
+export async function POST(
+    request: NextRequest,
+    { params }: { params: Promise<{ cmd: string }> },
+) {
+    const body = await request.json() || null;
+    if (!body.token || body.token !== process.env.SECRET_CODE) {
+        return NextResponse.json(
+            { error: "Unauthorized" },
+            { status: 401 },
+        );
+    }
+    try {
+        const { cmd } = await params;
+        if (!["auth", "ping"].includes(cmd)) {
+            return NextResponse.json(
+                { error: "Invalid command. Use: auth or ping" },
+                { status: 400 },
+            );
+        }
+        const command = cmd as testCommand;
+        switch (command) {
+            case "auth":
+                return NextResponse.json(
+                    { authStatus: "ok" },
+                    { status: 200 },
+                );
+
+            case "ping":
+                return NextResponse.json(
+                    { pingStatus: "ok" },
+                    { status: 200 },
+                )
+        }
+    } catch (error) {
+        return NextResponse.json({ error });
+    }
+}
