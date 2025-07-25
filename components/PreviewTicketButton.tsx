860bfdc21f83a7c46fc5171029332641e565e3d7
Tien Dat Pham <dat20036@gmail.com>
2025-07-25 20:59:59 +0700

    feat(tickets): WE SOLD OUT WOAH!!!!

--- components/PreviewTicketButton.tsx
+++ components/PreviewTicketButton.tsx
@@ -2,11 +2,37 @@ import Link from "next/link";
 
 export default function PreviewTicketButton() {
     return (
-        <Link
-            href={"/tickets"}
-            className={"btn btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"}
-        >
-            <div className={"font-bold"}>Mua vé</div>
-        </Link>
+        // yes, we don't have a CMS for now...
+        // <Link
+        //     href={"/tickets"}
+        //     className={"btn btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-black text-white hover:bg-white hover:text-black"}
+        // >
+        //     <div className={"font-bold"}>Mua vé</div>
+        // </Link>
+        <>
+            <button
+                type={"button"}
+                className={"btn btn-md lg:btn-lg rounded-xl bg-black text-white font-bold"}
+                onClick={() => (document.getElementById("sold-out-modal") as HTMLDialogElement)!.showModal()}
+            >
+                Mua vé
+            </button>
+            <dialog id={"sold-out-modal"} className={"modal"}>
+                <div className={"modal-box space-y-1"}>
+                    <div className={"font-bold text-xl text-center"}>Xin lỗi bạn nha, tụi mình soldout rồi...</div>
+                    <p>
+                        Bạn nhớ theo dõi thông tin mới nhất tại page
+                        {" "}
+                        <Link className={"font-extrabold underline"} href={"https://www.facebook.com/terrastationvn"}>Trạm dừng chân chốn Terra nhé.</Link>
+                    </p>
+                    <p>Tụi mình mong được gặp bạn tại Offline!</p>
+                    <p className={"text-center py-4 italic"}>(bấm ra bên ngoài để đóng)</p>
+                </div>
+                <form method={"dialog"} className={"modal-backdrop"}>
+                    {/* eslint-disable-next-line react-dom/no-missing-button-type */}
+                    <button>close</button>
+                </form>
+            </dialog>
+        </>
     );
 }
--- components/PurchaseTicketButton.tsx
+++ components/PurchaseTicketButton.tsx
@@ -1,12 +1,13 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function PurchaseTicketButton() {
     return (
-        <Link
-            href={process.env.NEXT_PUBLIC_FORM_LINK!}
-            className={"btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-white text-black hover:bg-black hover:text-white"}
-        >
-            <div className={"font-bold"}>Còn chờ cái gì mà không mua nữa chat.</div>
-        </Link>
+        // <Link
+        //     href={process.env.NEXT_PUBLIC_FORM_LINK!}
+        //     className={"btn sm:btn-sm md:btn-md lg:btn-lg hover:btn-outline rounded-2xl bg-white text-black hover:bg-black hover:text-white"}
+        // >
+        //     <div className={"font-bold"}>Còn chờ cái gì mà không mua nữa chat.</div>
+        // </Link>
+        <></>
     );
 }
